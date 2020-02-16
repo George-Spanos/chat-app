a9b6c7c4f741ab2c369966016390c8ac1b1f46e5
george spanos <gspanos@inttrust.gr>
2020-02-16 17:49:15 +0200

    node version log

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 const PORT = process.env.PORT || 3000;
+console.log('Port: ', PORT)
+console.log('Node Version', process.version);
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(PORT);
-  console.log('Listening to port '+ PORT)
+  console.log('Listening to port ' + PORT)
 }
 bootstrap();
